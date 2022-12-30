bbb4aa8ddca1e1e1363e0e0a00bf96b61dafca4d
Luis Ramírez <luera19@gmail.com>
2022-12-29 18:09:24 -0600

    Primeras clases MVC
    Primeras clases MVC con PHP

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once("vistas/template.php");
+
+?>
---
+++ ruteador.php
@@ -0,0 +1,8 @@
+<?php
+    include_once("controlador/controlador_paginas.php");
+
+    $controlador = new ControladorPaginas();
+    $controlador-> inicio();
+
+
+?>
